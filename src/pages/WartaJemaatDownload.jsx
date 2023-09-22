82e3b31a7306e8a898649ecdf8b5a4da62f009a0
Fernando Herman Pookey <nandopookey@gmail.com>
2023-09-22 16:40:33 +0700

    B

--- src/pages/WartaJemaatDownload.jsx
+++ src/pages/WartaJemaatDownload.jsx
@@ -3,76 +3,40 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const WartaJemaatDownload = () => {
-  // const [wartaJemaat, setWartaJemaat] = useState([]);
-  // const getWartaJemaat = async () => {
-  //   const response = await axios.get(
-  //     `${process.env.REACT_APP_GET_API}/wartajemaat`
-  //   );
-  //   setWartaJemaat(response.data);
-  // };
-
-  // useEffect(() => {
-  //   getWartaJemaat();
-  // }, []);
-
-  // const dispatch = useDispatch();
-
-  const [title, setTitle] = useState("");
+  const [judul, setJudul] = useState("");
   const [file, setFile] = useState("");
+  const [deskripsi, setDeskripsi] = useState("");
+  const [title, setTitle] = useState("");
   const [preview, setPreview] = useState("");
   const { id } = useParams();
-  // const navigate = useNavigate();
 
   useEffect(() => {
-    getSliderById();
+    getWartaJemaatById();
   }, []);
 
-  const getSliderById = async () => {
+  const getWartaJemaatById = async () => {
     const response = await axios.get(
       `${process.env.REACT_APP_GET_API}/wartajemaat/${id}`
     );
     setTitle(response.data.title);
-    setFile(response.data.document);
+    setFile(response.data.file);
     setPreview(response.data.url);
+
+    console.log(response.data);
   };
 
   const loadImage = (e) => {
-    const image = e.target.files[0];
-    setFile(image);
-    setPreview(URL.createObjectURL(image));
+    const document = e.target.files[0];
+    setFile(document);
+    setPreview(URL.createObjectURL(document));
   };
-
-  // const updateSlider = async (e) => {
-  //   e.preventDefault();
-  //   const formData = new FormData();
-  //   formData.append("file", file);
-  //   formData.append("title", title);
-  //   try {
-  //     await axios.patch(
-  //       `${process.env.REACT_APP_GET_API}/slider/${id}`,
-  //       formData
-  //     );
-  //     swal({
-  //       title: "Sukses",
-  //       text: "Data berhasil diubah",
-  //       icon: "success",
-  //       button: "Ok",
-  //     });
-  //     navigate("/slider");
-  //   } catch (error) {
-  //     if (error.response) {
-  //       setMsg(error.response.data.msg);
-  //     }
-  //   }
-  // };
 
   return (
     <div>
       <div>
         <h4>Detail Warta</h4>
         {/* <p>{title}</p> */}
-        {/* <embed src={document} type="application/pdf" /> */}
-        <iframe src={document} frameborder="0"></iframe>
+        <iframe src={preview} frameborder="0"></iframe>
       </div>
     </div>
   );
